de2f1353471282e11d34b3ca4a43b827df7b1c85
Aly-Tamboura <alytamboura7370@gmail.com>
2017-05-18 19:29:40 -0700

    commented out lyrics api request

--- server/app.js
+++ server/app.js
@@ -223,6 +223,9 @@ app.post('/sendlyrics', (req, res) => {
 
   let lyrics = [];
   var counter = 0;
+  ////////////////////////////////////////////
+   /// uncomment tomorrow when api works
+  ///////////////////////////////////////////
   // var getData = setInterval(function() {
   //   counter++
   //   console.log(counter)
